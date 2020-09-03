2f255e4c856b31f21f94c97f7bd0957c1b64bd3c
Anand Pawar <Anand.Pawar@cuelogic.local>
2020-09-03 09:45:35 +0530

    edit functionality modified for category and reminder fields

--- js/edittodo.js
+++ js/edittodo.js
@@ -18,6 +18,20 @@ window.addEventListener("DOMContentLoaded",function(){
     document.getElementById("inputTitle").value=tobeedittodo.title;
     document.getElementById("inputTargetDate").value=tobeedittodo.targetDate;
     document.getElementById("rd_public_"+tobeedittodo.isPublic).checked =true;
+    if(tobeedittodo.reminderDate){
+        document.getElementById("rd_reminder_yes").checked =true;
+        document.getElementById("div_reminderDt").style.display="block";
+        document.getElementById("inputReminderDate").value=tobeedittodo.reminderDate;
+    }
+    else{
+        document.getElementById("rd_reminder_no").checked =true;
+        document.getElementById("div_reminderDt").style.display="none";
+    }
+     for(var i = 0; i < tobeedittodo.categories.length; i++)
+     {
+        document.getElementById("chk_"+tobeedittodo.categories[i]).checked =true;
+     }
+
 
     document.getElementById("btnEditTodo").addEventListener("click",editTodo)
 })
@@ -28,15 +42,33 @@ function editTodo(e){
     const params = new URLSearchParams(window.location.search);
     var id=params.get("id");
     var users = JSON.parse(localStorage.getItem("userlist"));
-        var loggedinuser=JSON.parse(sessionStorage.getItem("loggedinuser"));
-        var todos=users.find( a => a.email == loggedinuser.email).todos;
+    var loggedinuser=JSON.parse(sessionStorage.getItem("loggedinuser"));
+    var todos=users.find( a => a.email == loggedinuser.email).todos;
+
+    var isReminder=document.querySelector('input[name="reminder"]:checked').value
+    var reminderDate;
+    if(isReminder=='yes'){
+        reminderDate=document.getElementById("inputReminderDate").value;
+    }
+    else{
+        reminderDate='';
+    }
+
+    var selected_Categories=document.querySelectorAll('input[name="inputCategory"]:checked');
+    var catVals = [];
+     for(var i = 0; i < selected_Categories.length; i++)
+     {
+        catVals.push(selected_Categories[i].value);
+     }
 
     var tobeupdatetodo={
         id:document.getElementById("inputId").value,
         title:document.getElementById("inputTitle").value,
         targetDate:document.getElementById("inputTargetDate").value,
         isDone:false,
-        isPublic:document.querySelector('input[name="public"]:checked').value
+        isPublic:document.querySelector('input[name="public"]:checked').value,
+        reminderDate:reminderDate,
+        categories:catVals
     }
     
     if(validatetodoform(tobeupdatetodo)){
@@ -44,8 +76,10 @@ function editTodo(e){
             if(todos[i].id==id){
                 todos[i].title=tobeupdatetodo.title;
                 todos[i].targetDate=tobeupdatetodo.targetDate;
-                todos[i].isDone=tobeupdatetodo.isDone
-                todos[i].isPublic=tobeupdatetodo.isPublic
+                todos[i].isDone=tobeupdatetodo.isDone;
+                todos[i].isPublic=tobeupdatetodo.isPublic;
+                todos[i].categories=tobeupdatetodo.categories;
+                todos[i].reminderDate=tobeupdatetodo.reminderDate;
                 break;
             }
         }
@@ -76,6 +110,10 @@ function validatetodoform(new_todo){
         isFormValid=false;
         document.getElementById("spn_targetdateinvalid").style.display="block";
     }
+    if(!new_todo.categories || new_todo.categories.length==0){
+        isFormValid=false;
+         document.getElementById("spn_categories").style.display="block";
+    }
     return isFormValid;
 }
 
@@ -101,4 +139,13 @@ function stringToDate(_date,_format,_delimiter)
             var formatedDate = new Date(dateItems[yearIndex],month,dateItems[dayIndex]);
             formatedDate.setHours(0, 0, 0, 0)
             return formatedDate;
+}
+
+function handleReminderClick(radioReminder){
+    if(radioReminder.value=='yes'){
+        document.getElementById("div_reminderDt").style.display="block";
+    }
+    else{
+        document.getElementById("div_reminderDt").style.display="none";
+    }
 }
